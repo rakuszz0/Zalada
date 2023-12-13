6623fa87bfa147d68a8dc522dd43be24463f7cc1
HafizRP23 <hafiz.tradersfamily@gmail.com>
2023-12-13 19:10:29 +0700

    add down queries on migration

--- ormconfig/migrations/1702464613720-drop-all-foreign-key.ts
+++ ormconfig/migrations/1702464613720-drop-all-foreign-key.ts
@@ -5,7 +5,7 @@ export class DropAllForeignKey1702464613720 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         // Drop foreign key carts product_id
         await queryRunner.query(`ALTER TABLE carts DROP FOREIGN KEY carts_ibfk_1`)
-        // Drop foreign key carts user_id
+        // Drop foreign key carts customer_id
         await queryRunner.query(`ALTER TABLE carts DROP FOREIGN KEY carts_ibfk_2`)
         // Drop foreign key orders product_id
         await queryRunner.query(`ALTER TABLE orders DROP FOREIGN KEY orders_ibfk_1`)
@@ -15,7 +15,7 @@ export class DropAllForeignKey1702464613720 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE trash_products DROP FOREIGN KEY trash_products_ibfk_1`)
         // Drop foreign key trash_user user_level
         await queryRunner.query(`ALTER TABLE trash_users DROP FOREIGN KEY trash_users_ibfk_1`)
-        // Drop foreign key user_group_rules rule_id
+        // Drop foreign key user_group_rules role_id
         await queryRunner.query(`ALTER TABLE user_group_rules DROP FOREIGN KEY user_group_rules_ibfk_1`)
         // Drop foreign key user_group_rules rules_id
         await queryRunner.query(`ALTER TABLE user_group_rules DROP FOREIGN KEY user_group_rules_ibfk_2`)
@@ -39,14 +39,14 @@ export class DropAllForeignKey1702464613720 implements MigrationInterface {
 
         // Add back foreign keys
         await queryRunner.query(`ALTER TABLE carts ADD FOREIGN KEY (product_id) REFERENCES products(id)`);
-        await queryRunner.query(`ALTER TABLE carts ADD FOREIGN KEY (user_id) REFERENCES users(id)`);
+        await queryRunner.query(`ALTER TABLE carts ADD FOREIGN KEY (customer_id) REFERENCES users(id)`);
         await queryRunner.query(`ALTER TABLE orders ADD FOREIGN KEY (product_id) REFERENCES products(id)`);
         await queryRunner.query(`ALTER TABLE products ADD FOREIGN KEY (store_id) REFERENCES stores(id)`);
         await queryRunner.query(`ALTER TABLE trash_products ADD FOREIGN KEY (store_id) REFERENCES stores(id)`);
-        await queryRunner.query(`ALTER TABLE trash_users ADD FOREIGN KEY (user_level) REFERENCES user_level(id)`);
-        await queryRunner.query(`ALTER TABLE user_group_rules ADD FOREIGN KEY (rule_id) REFERENCES rules(id)`);
-        await queryRunner.query(`ALTER TABLE user_group_rules ADD FOREIGN KEY (rules_id) REFERENCES rules(id)`);
-        await queryRunner.query(`ALTER TABLE users ADD FOREIGN KEY (user_level) REFERENCES user_level(id)`);
+        await queryRunner.query(`ALTER TABLE trash_users ADD FOREIGN KEY (user_level) REFERENCES user_roles(id)`);
+        await queryRunner.query(`ALTER TABLE user_group_rules ADD FOREIGN KEY (role_id) REFERENCES user_roles(id)`);
+        await queryRunner.query(`ALTER TABLE user_group_rules ADD FOREIGN KEY (rules_id) REFERENCES user_rules(id)`);
+        await queryRunner.query(`ALTER TABLE users ADD FOREIGN KEY (user_level) REFERENCES user_roles(id)`);
     }
 
 }
